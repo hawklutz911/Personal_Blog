6f5224ee45830927f438338b3d6390db32e9697d
Walter Lutsyk <waltlutsyk@gmail.com>
2021-11-08 10:46:02 -0700

    Initial commit.

--- .next/static/webpack/webpack.fcc22ac0f0e6bb9f.hot-update.js
+++ .next/static/webpack/webpack.fcc22ac0f0e6bb9f.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "15b319e3531daab7"; }
+/******/ 	__webpack_require__.h = function() { return "8250df2c19d035e8"; }
 /******/ }();
 /******/ 
 /******/ }
