8c23612ba28af808fd9a680d1f0afd491be2ac9f
Walter Lutsyk <waltlutsyk@gmail.com>
2021-11-13 20:21:58 -0700

    Edits3

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2986c881e29db660"; }
+/******/ 		__webpack_require__.h = function() { return "5b30d03029c4c8a1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
